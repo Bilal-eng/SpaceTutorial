85bfce63eda6a556994638b91ec901a4e2dbfc23
Bilal-eng <bilal.haliloglu90@gmail.com>
2026-09-24 10:05:13 +0300

    Prepare a Koin class for iOS dependency injection

---
+++ sharedLogic/src/iosMain/kotlin/com/bilal/spacetutorial/KoinHelper.kt
@@ -0,0 +1,30 @@
+package com.bilal.spacetutorial
+
+import com.bilal.spacetutorial.cache.IOSDatabaseDriverFactory
+import com.bilal.spacetutorial.entity.RocketLaunch
+import com.bilal.spacetutorial.network.SpaceApi
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+class KoinHelper : KoinComponent {
+    private val sdk: SpaceSDK by inject<SpaceSDK>()
+
+    suspend fun getLaunches(forceReload: Boolean): List<RocketLaunch> {
+        return sdk.getLaunches(forceReload = forceReload)
+    }
+}
+
+fun initKoin() {
+    startKoin {
+        modules(module {
+            single<SpaceApi> { SpaceApi() }
+            single<SpaceSDK> {
+                SpaceSDK(
+                    databaseDriverFactory = IOSDatabaseDriverFactory(), api = get()
+                )
+            }
+        })
+    }
+}
